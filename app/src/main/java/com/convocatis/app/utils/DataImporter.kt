d3dae5dc4928bc1b4f3fa06ccc23a89d7d821076
Aivars <aivarszar@gmail.com>
2025-10-26 00:02:04 +0300

    Labota Rožukronis virsrakstu + %RID struktūra

--- app/src/main/java/com/convocatis/app/utils/DataImporter.kt
+++ app/src/main/java/com/convocatis/app/utils/DataImporter.kt
@@ -17,7 +17,7 @@ class DataImporter(private val context: Context) {
 
     companion object {
         private const val TAG = "DataImporter"
-        private const val KEY_DATA_IMPORTED = "data_imported_v4"  // v4: Fixed Rožukronis text sequence (2024-10-25)
+        private const val KEY_DATA_IMPORTED = "data_imported_v5"  // v4: Fixed Rožukronis text sequence (2024-10-25)
     }
 
     /**
