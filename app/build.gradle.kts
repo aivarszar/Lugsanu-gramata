2b696cbd4d1e3b972436e3134a102f5e383a167c
Aivars <aivarszar@gmail.com>
2025-11-02 06:52:27 +0200

    formāta labojumi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.convocatis.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "2.4"
+        versionCode = 6
+        versionName = "2.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
